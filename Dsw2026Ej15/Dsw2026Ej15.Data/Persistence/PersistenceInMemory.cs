4f31795a279cf2805aa2db0416906fcffd4aa807
MiltonTolaba <Milton.Tolaba@alu.frt.utn.edu.ar>
2026-06-19 01:12:01 -0300

    Primeros puntos realizados y cambios en el Readme

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/Persistence/PersistenceInMemory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+using System.Text.Json;
+
+namespace Dsw2026Ej15.Data.Persistence
+{
+    public class PersistenceInMemory : IPersistence
+    {
+        public List<Doctor> Doctors { get; private set; }
+
+        public List<Speciality> Specialities { get; private set; }
+
+        public PersistenceInMemory()
+        {
+            Doctors = new List<Doctor>();
+            Specialities = LoadSpecialities();
+            Console.WriteLine($"Especialidades cargadas: {Specialities.Count}");
+        }
+
+        private List<Speciality> LoadSpecialities()
+        {
+            var json = File.ReadAllText("specialities.json");
+
+            return JsonSerializer.Deserialize<List<Speciality>>(json)
+                   ?? new List<Speciality>();
+        }
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Interfaces/IPersistence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Ej15.Domain.Entities;
+
+namespace Dsw2026Ej15.Domain.Interfaces
+{
+    public interface IPersistence
+    {
+        List<Doctor> Doctors { get; }
+        List<Speciality> Specialities { get; }
+    }
+}
